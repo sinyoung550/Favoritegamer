d8266a81139298603c4071a70aaed2f5eebb4be5
sinyoung550 <sinyoung550@gamil.com>
2017-03-30 20:57:46 +0900

    #4 Complete Button Event Programming

--- app/src/main/java/com/example/tlsdu/favoritegamer/MainActivity.java
+++ app/src/main/java/com/example/tlsdu/favoritegamer/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
     CheckBox checkSelect;
     TextView textQuest;
     RadioGroup rg;//멤버필드로 선언
@@ -35,8 +36,10 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         butok=((Button)findViewById(R.id.but_ok));
         imggamer=((ImageView)findViewById(R.id.imgv_gamer));
         //체크박스에 이벤트 감시자
-        checkSelect.setOnCheckedChangeListener(this);//MainActicity를 핸들러로 만드는 방법
-    }
+        checkSelect.setOnCheckedChangeListener(this);//MainActicity를 핸들러로 만드는 방법으로 구현
+        butok.setOnClickListener(this);
+
+    }//end onCreate
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -51,6 +54,33 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             rg.setVisibility(View.INVISIBLE);
             butok.setVisibility(View.INVISIBLE);
             imggamer.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (rg.getCheckedRadioButtonId()){//getCheckedRadioButtonId()<<체크된 라디오버튼의 아이디를 반환해주는 메소드
+            case R.id.radio_injae:
+                imggamer.setImageResource(R.drawable.injae); //제일 간단한 방법
+                break;
+            case R.id.radio_ryu:
+                imggamer.setImageResource(R.drawable.ryu);
+                break;
+            case R.id.radio_whoru:
+                imggamer.setImageResource(R.drawable.whoru);
+                break;case R.id.radio_miro:
+                imggamer.setImageResource(R.drawable.miro);
+                break;case R.id.radio_zunba:
+                imggamer.setImageResource(R.drawable.zunba);
+                break;
+            case R.id.radio_tobi:
+                imggamer.setImageResource(R.drawable.tobib);
+                break;
+            case R.id.radio_lunatic:
+                imggamer.setImageResource(R.drawable.lunatic);
+                break;
+            default:
+                Toast.makeText(this,"아무것도 선택되지 않았습니다.",Toast.LENGTH_SHORT).show();
         }
     }
 }
